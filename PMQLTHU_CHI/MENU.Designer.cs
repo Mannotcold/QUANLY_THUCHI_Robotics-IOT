3056a6ad9b257b16ba2f2cc472573f38cbbfdd20
Mannotcold <man2082002@gmail.com>
2023-05-18 01:56:55 +0700

    update

--- PMQLTHU_CHI/MENU.Designer.cs
+++ PMQLTHU_CHI/MENU.Designer.cs
@@ -29,29 +29,30 @@ namespace PMQLTHU_CHI
         /// </summary>
         private void InitializeComponent()
         {
-            this.dgvDSLop = new System.Windows.Forms.DataGridView();
+            this.dgvPhieuTC = new System.Windows.Forms.DataGridView();
             this.btnTraCuu = new System.Windows.Forms.Button();
             this.txtTuKhoa = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvDSLop)).BeginInit();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPhieuTC)).BeginInit();
             this.SuspendLayout();
             // 
-            // dgvDSLop
+            // dgvPhieuTC
             // 
-            this.dgvDSLop.AllowUserToAddRows = false;
-            this.dgvDSLop.AllowUserToDeleteRows = false;
-            this.dgvDSLop.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.dgvPhieuTC.AllowUserToAddRows = false;
+            this.dgvPhieuTC.AllowUserToDeleteRows = false;
+            this.dgvPhieuTC.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dgvDSLop.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dgvDSLop.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvDSLop.Location = new System.Drawing.Point(35, 102);
-            this.dgvDSLop.MultiSelect = false;
-            this.dgvDSLop.Name = "dgvDSLop";
-            this.dgvDSLop.ReadOnly = true;
-            this.dgvDSLop.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvDSLop.Size = new System.Drawing.Size(769, 474);
-            this.dgvDSLop.TabIndex = 11;
+            this.dgvPhieuTC.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvPhieuTC.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPhieuTC.Location = new System.Drawing.Point(35, 102);
+            this.dgvPhieuTC.MultiSelect = false;
+            this.dgvPhieuTC.Name = "dgvPhieuTC";
+            this.dgvPhieuTC.ReadOnly = true;
+            this.dgvPhieuTC.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPhieuTC.Size = new System.Drawing.Size(769, 474);
+            this.dgvPhieuTC.TabIndex = 11;
             // 
             // btnTraCuu
             // 
@@ -63,6 +64,7 @@ namespace PMQLTHU_CHI
             this.btnTraCuu.TabIndex = 10;
             this.btnTraCuu.Text = "Tra cứu";
             this.btnTraCuu.UseVisualStyleBackColor = true;
+            this.btnTraCuu.Click += new System.EventHandler(this.btnTraCuu_Click);
             // 
             // txtTuKhoa
             // 
@@ -84,18 +86,34 @@ namespace PMQLTHU_CHI
             this.label1.TabIndex = 8;
             this.label1.Text = "Tìm kiếm";
             // 
+            // guna2Button1
+            // 
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(35, 62);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(110, 22);
+            this.guna2Button1.TabIndex = 12;
+            this.guna2Button1.Text = "Thêm mới";
+            // 
             // MENU
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(844, 598);
-            this.Controls.Add(this.dgvDSLop);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.dgvPhieuTC);
             this.Controls.Add(this.btnTraCuu);
             this.Controls.Add(this.txtTuKhoa);
             this.Controls.Add(this.label1);
             this.Name = "MENU";
             this.Text = "MENU";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvDSLop)).EndInit();
+            this.Load += new System.EventHandler(this.MENU_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPhieuTC)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,9 +121,10 @@ namespace PMQLTHU_CHI
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dgvDSLop;
+        private System.Windows.Forms.DataGridView dgvPhieuTC;
         private System.Windows.Forms.Button btnTraCuu;
         private System.Windows.Forms.TextBox txtTuKhoa;
         private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
     }
 }
--- PMQLTHU_CHI/MENU.cs
+++ PMQLTHU_CHI/MENU.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace PMQLTHU_CHI
 {
@@ -15,6 +16,50 @@ namespace PMQLTHU_CHI
         public MENU()
         {
             InitializeComponent();
+        }
+
+        //Ket noi server sql
+        SqlConnection connection;
+        SqlCommand command;
+        string str = "Data Source=.;Initial Catalog=QLTC;Integrated Security=True";
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable table = new DataTable();
+
+        void loaddata()
+        {
+            connection = new SqlConnection(str);
+            connection.Open();
+            command = connection.CreateCommand();
+            command.CommandText = "select * from PHIEU_THU_CHI";
+            adapter.SelectCommand = command;
+            table.Clear();
+            adapter.Fill(table);
+            dgvPhieuTC.DataSource = table;
+            connection.Close();
+        }
+        private void btnTraCuu_Click(object sender, EventArgs e)
+        {
+            if (txtTuKhoa.Text == "")
+            {
+                loaddata();
+            }
+            else
+            {
+                connection = new SqlConnection(str);
+                connection.Open();
+                string TenKH = txtTuKhoa.Text;
+                command = connection.CreateCommand();
+                command.CommandText = "select * from PHIEU_THU_CHI WHERE Nguoi = '" + TenKH + "'";
+                adapter.SelectCommand = command;
+                table.Clear();
+                adapter.Fill(table);
+                dgvPhieuTC.DataSource = table;
+            }
+        }
+
+        private void MENU_Load(object sender, EventArgs e)
+        {
+            loaddata();
         }
     }
 }
