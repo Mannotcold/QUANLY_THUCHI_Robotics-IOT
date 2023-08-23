008bbd5387cc7a93c43d53086820a87586915c0f
Mannotcold <man2082002@gmail.com>
2023-08-23 20:12:20 +0700

    up

--- QLTC.sql
+++ QLTC.sql
@@ -10,10 +10,6 @@ CREATE TABLE TaiKhoan
    MATKHAU char(30),
    PRIMARY KEY (TEN_TK)
 );
-insert into TaiKhoan (TEN_TK, MATKHAU) 
-values 
-('dhthuong','dhthuong123')
-
 
 CREATE TABLE KhoaHoc
 (
@@ -21,14 +17,6 @@ CREATE TABLE KhoaHoc
    PRIMARY KEY (KhoaHoc)
 );
 
-insert into KhoaHoc (KhoaHoc) 
-values 
-('KH001'),
-('KH002'),
-('KH003'),
-('KH004');
-
-select * from KhoaHoc
 
 CREATE TABLE LopHoc
 (
@@ -36,17 +24,6 @@ CREATE TABLE LopHoc
    PRIMARY KEY (LopHoc)
 );
 
-insert into LopHoc (LopHoc) 
-values 
-('LH001'),
-('LH002'),
-('LH003'),
-('LH004');
-
-update LopHoc set LopHoc = 'LH002' Where LopHoc = 'KH002'
-
-select * from LopHoc
-drop table PHIEU_THU_CHI
 
 
 CREATE TABLE PHIEU_THU_CHI
@@ -66,8 +43,27 @@ CREATE TABLE PHIEU_THU_CHI
 );
 
 
-select * from PHIEU_THU_CHI
-drop TABLE PHIEU_THU_CHI
+
+insert into TaiKhoan (TEN_TK, MATKHAU) 
+values 
+('dhthuong','dhthuong123')
+
+insert into KhoaHoc (KhoaHoc) 
+values 
+('KH001'),
+('KH002'),
+('KH003'),
+('KH004');
+
+select * from KhoaHoc
+
+insert into LopHoc (LopHoc) 
+values 
+('LH001'),
+('LH002'),
+('LH003'),
+('LH004');
+
 
 insert into PHIEU_THU_CHI (NgayLap,KhoaHoc,LopHoc,Thu, Chi, Nguoi, Cash, Discount ,SoHoaDon) 
 values 
@@ -76,3 +72,8 @@ values
 ( '2023-02-07','KH003','LH003',null,2245000,N'LAM',N'Chuyển khoản','20','5565476556'),
 ( '2023-11-07','KH004','LH004',null,2245000,N'TRACH',N'Tiền mặt','20','56556565556');
 
+select NgayLap AS Ngày_lập,KhoaHoc AS Khóa_học ,
+LopHoc AS Lớp_học ,FORMAT(thu, '#,##0') AS Thu,FORMAT(chi, '#,##0') AS Chi,
+Nguoi AS Khách_hàng, Cash AS Thanh_toán,Discount as Giảm_giá, SoHoaDon AS Số_hóa_đơn 
+from PHIEU_THU_CHI 
+WHERE Nguoi LIKE 'Man' OR KhoaHoc LIKE 'KH001' 
